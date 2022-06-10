5e34012dc180a4935daaf58de166c0f6b867e363
bpolasek519 <106794202+bpolasek519@users.noreply.github.com>
2022-06-09 22:19:10 -0500

    Add files via upload
    Finished part 5

--- Challenge 1/Starter_Code/Starter_Code/loan_analyzer.py
+++ Challenge 1/Starter_Code/Starter_Code/loan_analyzer.py
@@ -181,26 +181,32 @@ for price in loans:
 print(f"The loans that cost less than $500 are {inexpensive_loans}.")
 
 
-# """Part 5: Save the results.
+"""Part 5: Save the results.
 
-# Output this list of inexpensive loans to a csv file
-#     1. Use `with open` to open a new CSV file.
-#         a. Create a `csvwriter` using the `csv` library.
-#         b. Use the new csvwriter to write the header variable as the first row.
-#         c. Use a for loop to iterate through each loan in `inexpensive_loans`.
-#             i. Use the csvwriter to write the `loan.values()` to a row in the CSV file.
+Output this list of inexpensive loans to a csv file
+    1. Use `with open` to open a new CSV file.
+        a. Create a `csvwriter` using the `csv` library.
+        b. Use the new csvwriter to write the header variable as the first row.
+        c. Use a for loop to iterate through each loan in `inexpensive_loans`.
+            i. Use the csvwriter to write the `loan.values()` to a row in the CSV file.
 
-#     Hint: Refer to the official documentation for the csv library.
-#     https://docs.python.org/3/library/csv.html#writer-objects
+    Hint: Refer to the official documentation for the csv library.
+    https://docs.python.org/3/library/csv.html#writer-objects
 
-# """
+"""
 
-# # Set the output header
-# header = ["loan_price", "remaining_months", "repayment_interval", "future_value"]
+# Set the output header
+header = ["loan_price", "remaining_months", "repayment_interval", "future_value"]
 
-# # Set the output file path
-# output_path = Path("inexpensive_loans.csv")
+# Set the output file path
+output_path = Path("inexpensive_loans.csv")
 
-# # @TODO: Use the csv library and `csv.writer` to write the header row
-# # and each row of `loan.values()` from the `inexpensive_loans` list.
-# # YOUR CODE HERE!
+# @TODO: Use the csv library and `csv.writer` to write the header row
+# and each row of `loan.values()` from the `inexpensive_loans` list.
+# YOUR CODE HERE!
+csvpath = Path("inexpensive_loans.csv")
+with open (csvpath, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    for row in inexpensive_loans:
+            csvwriter.writerow(header)
+            csvwriter.writerow(row.values())
